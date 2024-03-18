7a731d6d97c12efdd2bc89e67dbfdc555369c405
rahman-cs-it <siddiquewasifur786123@gmail.com>
2024-03-18 20:08:46 +0530

    successfully solve the api error

--- projects/Currency_converter/currency.js
+++ projects/Currency_converter/currency.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json";
+    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
 
 
@@ -10,6 +10,7 @@ const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
+const swap = document.querySelector("#swap");
 
 for (let select of dropdowns) {
   for (currCode in countryList) {
@@ -29,20 +30,45 @@ for (let select of dropdowns) {
   });
 }
 
+const swapping = () => {
+  // Swap the values of fromCurr and toCurr select elements
+  
+  const temp = fromCurr.value;
+  fromCurr.value = toCurr.value;
+  toCurr.value = temp;
+
+  // Update the flag images based on the new selections
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
+
+  // Update the exchange rate based on the new selections
+  updateExchangeRate();
+};
+
+swap.addEventListener( "click", swapping );
+
 const updateExchangeRate = async () => {
+  
   let amount = document.querySelector(".amount input");
   let amtVal = amount.value;
+  
   if (amtVal === "" || amtVal < 1) {
     amtVal = 1;
     amount.value = "1";
   }
-  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  // console.log(amount.value);
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
   let response = await fetch(URL);
   let data = await response.json();
-  let rate = data[toCurr.value.toLowerCase()];
-
+  // console.log(amount.value)
+  
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+  
+  // console.log(amount.value);
   let finalAmount = amtVal * rate;
+  // console.log(amount.value);
   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  // console.log(amount.value);
 };
 
 const updateFlag = (element) => {
