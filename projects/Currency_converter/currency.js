b9ad25b2ebfc20da5045ade1a0ab82dadc80e766
rahman-cs-it <siddiquewasifur786123@gmail.com>
2024-03-18 18:38:53 +0530

    updating api

--- projects/Currency_converter/currency.js
+++ projects/Currency_converter/currency.js
@@ -1,6 +1,7 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-  https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/{apiVersion}/{endpoint}
+    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json";
+
+
 
 
 
